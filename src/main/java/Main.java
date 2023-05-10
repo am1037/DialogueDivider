624e5ee415c3f265d56ca8861a0c776280089a86
am1037 <alsrlalsrl@gmail.com>
2023-05-10 17:43:31 +0900

    괄호 입력 기능

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,18 @@ public class Main {
         System.out.println("Enter the index of the file you want to read: ");
         int index = sc.nextInt();
         File file = texts[index];
+        System.out.println("Enter opener and closer: ");
+        char dopen = '"';
+        char dclose = '"';
+        try{
+            dopen = sc.next().charAt(0);
+            dclose = sc.next().charAt(0);
+        }
+        catch (Exception e){
+            System.out.println("No opener and closer entered. Using default.");
+        }
 
-        Text text = new Text(file, '『', '』');
+        Text text = new Text(file, dopen, dclose);
         System.out.println(text.getTitle());
         for (Content content : text.getContents()) {
             System.out.println(content);

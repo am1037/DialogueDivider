74ab158be00f1ed56e4b4c4ed6bdf77d7bae840a
am1037 <alsrlalsrl@gmail.com>
2023-05-10 16:44:43 +0900

    index 지정할 수 있도록 간단한 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         File[] texts = new File("texts\\").listFiles();
-        File file = texts[0];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < texts.length; i++) {
+            System.out.println(i + " " + texts[i].getName());
+        }
+        System.out.println("Enter the index of the file you want to read: ");
+        int index = sc.nextInt();
+        File file = texts[index];
 
         Text text = new Text(file);
         System.out.println(text.getTitle());
